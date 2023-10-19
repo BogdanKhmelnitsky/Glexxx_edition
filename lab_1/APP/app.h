7ab1732ff793f2426366be2babb5da1e2fdf71c3
BogdanKhmelnitsky <113645166+BogdanKhmelnitsky@users.noreply.github.com>
2023-10-19 12:24:37 +0300

    comit

---
+++ lab_1/APP/app.h
@@ -0,0 +1,15 @@
+#ifndef APP_H
+#define APP_H
+
+/* Pablic Type definition */
+// Enumerators
+// Structures
+// New type Varaible
+// Constants
+
+/* Functions definition */
+
+void app_init(void);
+void app_run(void);
+
+#endif /* APP_H */
